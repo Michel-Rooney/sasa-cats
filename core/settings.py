ae51e8d055875860cabc62a6ac80895cb5b754f7
Michel-Rooney <michelrooney16@gmail.com>
2023-08-24 23:38:27 -0300

    Deploy

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,14 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Whitenoise
-STATICFILES_STORAGES = "whitenoise.storage.CompressedManifestStaticFilesStorage"  # noqa: E501
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  # noqa: E501
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
